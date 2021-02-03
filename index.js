2947aad7fef9917b377a1881b0be21838aae0644
Alamgir Ahamed Saif <36258593+alamgir72435@users.noreply.github.com>
2021-02-03 11:42:51 +0600

    show result on response

--- index.js
+++ index.js
@@ -1,10 +1,10 @@
 const app = require("express")();
 const PDFDocument = require("pdfkit");
 const fs = require("fs");
-
+const path = require("path");
 function createBanglaPdf(content) {
   let doc = new PDFDocument();
-  doc.pipe(fs.createWriteStream("output.pdf"));
+  doc.pipe(fs.createWriteStream("public/report.pdf"));
   doc.font("fonts/kalpurush.ttf").fontSize(14);
   doc.text(content, 100, 100);
   doc.end();
@@ -15,5 +15,9 @@ createBanglaPdf(`
   সারা বিশ্বের বিস্ময় তুমি আমার অহংকার!
   এবং আমরা একটি সফটওয়্যার বানাচ্ছি
 `);
+
+app.get("/", (req, res) => {
+  res.sendFile(path.join(__dirname, "public", "report.pdf"));
+});
 
 app.listen(5000, console.log(`Running`));
