74e41ace979b48505d779016e3390866ce0d1859
Alamgir Ahamed Saif <36258593+alamgir72435@users.noreply.github.com>
2021-02-03 12:12:23 +0600

    solution for generate

--- index.js
+++ index.js
@@ -2,22 +2,27 @@ const app = require("express")();
 const PDFDocument = require("pdfkit");
 const fs = require("fs");
 const path = require("path");
-function createBanglaPdf(content) {
-  let doc = new PDFDocument();
+
+const makeSalesSummery = new Promise((resolve, reject) => {
+  let doc = new PDFDocument({ bufferPages: true });
   doc.pipe(fs.createWriteStream("public/report.pdf"));
   doc.font("fonts/kalpurush.ttf").fontSize(14);
-  doc.text(content, 100, 100);
-  doc.end();
-}
-
-createBanglaPdf(`
-  একটি বাংলাদেশ তুমি জাগ্রত জনতার,
+  doc.text(
+    `একটি বাংলাদেশ তুমি জাগ্রত জনতার,
   সারা বিশ্বের বিস্ময় তুমি আমার অহংকার!
-  এবং আমরা একটি সফটওয়্যার বানাচ্ছি
-`);
+  এবং আমরা একটি সফটওয়্যার বানাচ্ছি !`,
+    100,
+    100
+  );
+  doc.end();
+  resolve();
+});
 
-app.get("/", (req, res) => {
-  res.sendFile(path.join(__dirname, "public", "report.pdf"));
+app.get("/", async (req, res) => {
+  await makeSalesSummery;
+  setTimeout(() => {
+    res.sendFile(path.join(__dirname, "public", "report.pdf"));
+  }, 100);
 });
 
 app.listen(5000, console.log(`Running`));
